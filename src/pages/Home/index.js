7a63e47fc4ca542b9fea08b077237300f8063485
unknown <eliaslima900@hotmail.com>
2020-12-21 12:28:23 -0300

    tirando rota background

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,10 @@
 import './index.css'
 import Navbar from '../../core/components/Navbar'
-import backgroundImage from "../../images/backgroundImage.jpg"
 
 function Home() {
   return (
     <div>
       <div className="backgroundImage">
-      <img src={backgroundImage} alt="Logo" />
         < Navbar />
         <div className="st-image">
           <img className="mb-4" src='/images/Beauty.png' alt="" width="400" height="200" />
